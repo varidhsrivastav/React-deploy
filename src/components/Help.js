9cc64a07695bef7d02956486084a20dda1202728
Varidh Srivastav <varidhsrivastava19145@gmail.com>
2022-07-19 12:28:51 +0530

    second commit

--- src/components/Help.js
+++ src/components/Help.js
@@ -6,9 +6,9 @@ const Help = () => {
   return (
     <>
       <section className="help  container-fluid">
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 319">
+      {/* <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 319">
   <path fill="#5D05A4" fill-opacity="1" d="M0,256L80,224C160,192,320,128,480,112C640,96,800,128,960,154.7C1120,181,1280,203,1360,213.3L1440,224L1440,0L1360,0C1280,0,1120,0,960,0C800,0,640,0,480,0C320,0,160,0,80,0L0,0Z"></path>
-</svg>
+</svg> */}
         <div className="">
           <div className="container">
             <div className="row">
